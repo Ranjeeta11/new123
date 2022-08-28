cd87d8334dc717ce538ed472704db66c1a69efe3
Ranjeeta11 <ranjeeta.s.rruproy@gmail.com>
2022-08-28 12:31:08 +0530

    Hi

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module project_rr_1 {
+}
---
+++ src/project_rr_1/java_1.java
@@ -0,0 +1,10 @@
+package project_rr_1;
+
+public class java_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("modified version");
+	}
+
+}
